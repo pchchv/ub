e9880f0d333b5628b248fcf0b64300948ed6b4e2
pchchv <ipchchv@gmail.com>
2023-03-13 12:16:25 +0300

    Implement a create user test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPasswordHash(t *testing.T) {
 	pass := "qewhy#fcu3!rt"
@@ -17,6 +19,35 @@ func TestCheckPasswordHash(t *testing.T) {
 		t.Fatal()
 	}
 	if !checkPasswordHash(pass, hash) {
+		t.Fatal()
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	jsonMap := make(map[string]interface{})
+	email := "ipchchv@gmail.com"
+	name := "Jack"
+	password := "3223414r"
+
+	jsonMap["email"] = email
+	jsonMap["name"] = name
+	jsonMap["password"] = password
+
+	user, err := createUser(jsonMap)
+	if err != nil {
+		t.Fatal()
+	}
+
+	if user.Email != email {
+		t.Fatal()
+	}
+	if user.Balance != 0 {
+		t.Fatal()
+	}
+	if user.Name != name {
+		t.Fatal()
+	}
+	if !checkPasswordHash(password, user.PassHash) {
 		t.Fatal()
 	}
 }
