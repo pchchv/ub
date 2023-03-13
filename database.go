cac11b17d69c98326a8fb853e09ee61e02989192
pchchv <ipchchv@gmail.com>
2023-03-13 16:15:45 +0300

    Add uuid support to pgx

--- database.go
+++ database.go
@@ -5,14 +5,27 @@ import (
 	"fmt"
 	"os"
 
+	pgxuuid "github.com/jackc/pgx-gofrs-uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pchchv/golog"
 )
 
 func db() {
-	conn, err := pgx.Connect(context.Background(), getEnvValue("DATABASE_URL"))
+	databaseURL := getEnvValue("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), databaseURL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
+
+	dbconfig, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		golog.Panic(err.Error())
+	}
+	dbconfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		pgxuuid.Register(conn.TypeMap())
+		return nil
+	}
 }
--- go.mod
+++ go.mod
@@ -9,16 +9,20 @@ require (
 )
 
 require (
+	github.com/gofrs/uuid/v5 v5.0.0 // indirect
 	github.com/influxdata/tdigest v0.0.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 )
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx-gofrs-uuid v0.0.0-20230224015001-1d428863c2e2
 	github.com/jackc/pgx/v5 v5.3.1
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
